b1a6a4f893b9ceeaaae7d859f69c6ebfa2daa396
_Jefta_ <Jefta.deLeeuw@web.de>
2020-08-06 14:07:28 +0200

    File for the trayicon to show up

---
+++ trayicon.py
@@ -0,0 +1,24 @@
+import pystray
+from PIL import Image, ImageDraw
+
+item = pystray.MenuItem
+
+def restore_console():
+    print ("Button clicked")
+
+def create ():
+
+    file = open (".files\logoDIR.txt", "r")
+    logoDIR = file.read()
+
+    trayicon = pystray.Icon('GA2W-Traymenu', create_image(logoDIR), menu=pystray.Menu(
+        pystray.MenuItem ('Restore Console', restore_console)))
+
+    trayicon.run()
+
+def create_image(image):
+    image = Image.open (image)
+    return image
+
+if __name__ == '__main__':
+    create()
