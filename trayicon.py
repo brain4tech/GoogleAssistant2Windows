cdbd435abb4c85dbfec019c1fd8d9b48c5143a1a
_Jefta_ <Jefta.deLeeuw@web.de>
2020-08-06 17:00:13 +0200

    Logofile is in .files now (local)

--- trayicon.py
+++ trayicon.py
@@ -1,26 +1,27 @@
 import pystray
+import os
 from PIL import Image, ImageDraw
-
-
-#import SysTrayIcon by Simon Brunning
-import SysTrayIcon
 
 def restore_console():
     print ("Button clicked")
 
 def create ():
 
-    file = open (".files\logoDIR.txt", "r")
-    logoDIR = file.read()
+    cwd = os.getcwd()
+    iconpath = cwd + "\\.files\\GA2W-logo.png"
 
-    trayicon = pystray.Icon('GA2W-Traymenu', create_image(logoDIR), menu=pystray.Menu(
+    trayicon = pystray.Icon('GA2W-Traymenu', create_image(iconpath), menu=pystray.Menu(
         pystray.MenuItem ('Restore Console', restore_console)))
 
     trayicon.run()
 
-def create_image(image):
-    image = Image.open (image)
+def create_image(imagepath):
+    image = Image.open (imagepath)
     return image
+
+def stop():
+    trayicon.stop()
+
 
 if __name__ == '__main__':
     create()
