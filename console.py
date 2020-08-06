a5394b6aa81e4b32d7964178d471e0b71be3d1b6
_Jefta_ <Jefta.deLeeuw@web.de>
2020-08-06 23:15:36 +0200

    Multiprocessing complete, hiding and restoring working properly

--- console.py
+++ console.py
@@ -1,40 +1,63 @@
-#Modules for the console
+#Import modules
 import cmd
 import ctypes
-import threading
+import multiprocessing
 import trayicon
 from sys import exit as exit
 
+kernel32 = ctypes.WinDLL('kernel32')
+user32 = ctypes.WinDLL('user32')
 
-#Change the title of the console
-ctypes.windll.kernel32.SetConsoleTitleW("GoogleAssistant2Windows Console")
+SW_HIDE = 0
+
+global hWnd
+hWnd = kernel32.GetConsoleWindow()
 
 class console(cmd.Cmd):
-    intro = "Dies ist eine Testkonsole für den GoogleAssistant2Windows \n"
+    intro = "--- Console for GoogleAssistant2Windows | Type >help< for more ---\n"
     prompt = ''
 
     def do_quit (self, arg):
         log ("Console ist shutting down.")
-        print ("going to join")
-        trayicon_thread.join()
-        print ("joined")
-        #exit()
+        trayicon_process.terminate()
         return True
 
+    def do_q (self, arg):
+        log ("Console ist shutting down.")
+        trayicon_process.terminate()
+        return True
+
+    def do_stop (self, arg):
+        log ("Console ist shutting down.")
+        trayicon_process.terminate()
+        return True
 
     def do_hide (self, arg):
+        user32.ShowWindow(hWnd, SW_HIDE)
         log ("Hid the console")
-
-    def do_shell (self, arg):
-        print (arg)
 
 def log (message):
     print (message)
 
 def restore_console ():
     print ("Console Restored")
+    user32.ShowWindow(hWnd)
 
-trayicon_thread = threading.Thread (daemon=True, target=trayicon.create)
-trayicon_thread.start ()
 
-console().cmdloop()
+
+def main (): #the main fuction to start
+
+    #change the title of the console
+    ctypes.windll.kernel32.SetConsoleTitleW("GoogleAssistant2Windows Console")
+
+    #Spawn a new process for the trayicon, as trayicon.create contains a run() statements,
+    #which blocks the programm
+    global trayicon_process
+    trayicon_process = multiprocessing.Process (target=trayicon.create)
+    trayicon_process.start ()
+
+    #Start the loop
+    console().cmdloop()
+
+if __name__ == '__main__':
+    main ()
--- trayicon.py
+++ trayicon.py
@@ -1,9 +1,7 @@
 import pystray
 import os
+import console
 from PIL import Image, ImageDraw
-
-def restore_console():
-    print ("Button clicked")
 
 def create ():
 
@@ -11,17 +9,13 @@ def create ():
     iconpath = cwd + "\\.files\\GA2W-logo.png"
 
     trayicon = pystray.Icon('GA2W-Traymenu', create_image(iconpath), menu=pystray.Menu(
-        pystray.MenuItem ('Restore Console', restore_console)))
+        pystray.MenuItem ('Restore Console', console.restore_console)))
 
     trayicon.run()
 
 def create_image(imagepath):
     image = Image.open (imagepath)
     return image
-
-def stop():
-    trayicon.stop()
-
 
 if __name__ == '__main__':
     create()
