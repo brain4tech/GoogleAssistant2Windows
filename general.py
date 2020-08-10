7153559dd1f9fafcdef7a0a3679e120aafda1159
_Jefta_ <Jefta.deLeeuw@web.de>
2020-08-10 10:52:37 +0200

    updated; msg_write isn't needed anymore

--- general.py
+++ general.py
@@ -3,14 +3,3 @@ from os import rename
 
 def current_time ():
     return strftime("%H:%M:%S", gmtime())
-
-def msg_write (message, time=0):
-    f = file.open (".files\\console-msg-clear.txt", a)
-    f.write (message)
-
-    if time != 0:
-        f.write (time)
-
-    f.close ()
-
-    rename (".files\\console-msg-clear.txt", ".files\\console-msg.txt")
