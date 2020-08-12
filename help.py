d4176b5244f899659ed58ff1e71554ed7939ed89
_Jefta_ <Jefta.deLeeuw@web.de>
2020-08-12 15:26:42 +0200

    script for help-strings

---
+++ help.py
@@ -0,0 +1,6 @@
+def help_main ():
+    help = [">   The following general commands are documented:",
+        "  quit\tShuts down the application",
+        "  hide\tHides the console and creates a trayicon.\n  \tYou can restore the console by right-clicking on this trayicon.",
+        "  clear\tClears the console output"]
+    return help
