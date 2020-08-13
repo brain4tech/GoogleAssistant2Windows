83ebf428535a5c0065832d7f685a3d726b441ab9
_Jefta_ <Jefta.deLeeuw@web.de>
2020-08-13 15:11:12 +0200

    Improved date and time functions

--- general.py
+++ general.py
@@ -1,5 +1,18 @@
 from time import strftime, gmtime
-from os import rename
 
-def current_time ():
-    return strftime("%H:%M:%S", gmtime())
+from datetime import date, datetime
+
+def current_time (symbol=":"):
+    now = datetime.now()
+    str_now = now.strftime("%H" + symbol + "%M" + symbol + "%S")
+
+    return str_now
+
+def current_date (symbol="-"):
+    today = date.today()
+    str_today = today.strftime("%d" + symbol + "%m" + symbol + "%Y")
+
+    return str_today
+
+def today ():
+    return current_date() + " " + current_time()
