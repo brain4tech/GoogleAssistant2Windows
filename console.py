0adb83c1896a8d80201b71761cd30fad9ae7972f
_Jefta_ <Jefta.deLeeuw@web.de>
2020-08-13 15:12:10 +0200

    Added event-manager and logging-mechanism

--- console.py
+++ console.py
@@ -2,6 +2,7 @@
 import tkinter
 from general import  *
 from queue import Queue
+from threading import Thread
 import help
 
 #import libraries for trayicon
@@ -9,7 +10,7 @@ import pystray
 import os
 from PIL import Image, ImageDraw
 
-q = Queue ()
+q_output = Queue ()
 
 """ Console UI """
 def create_console_GUI ():
@@ -29,7 +30,7 @@ def create_console_GUI ():
     console_output = tkinter.Text (consoleGUI, bg="white", height=15, bd=1,
         font=console_font, borderwidth = 1, relief="solid")
     console_output.configure(state='disabled')
-    q.put(console_output)
+    q_output.put(console_output)
 
     console_prompt = tkinter.Entry (consoleGUI, bd=0, font=console_font, borderwidth = 1, relief="solid")
 
@@ -46,7 +47,7 @@ def start_console_GUI():
 
 """ Console functionality """
 
-def log (message, mprefix=0, time=0, userinput=False, guitext=0):
+def console_log (message, mprefix=0, time=0, userinput=False, guitext=0):
     if time == 0:
         time = current_time()
 
@@ -71,7 +72,7 @@ def log (message, mprefix=0, time=0, userinput=False, guitext=0):
 
 def log_array (array):
     for x in array:
-        log (x)
+        console_log (x)
 
 def process_cmd(event):
     cmd = console_prompt.get()
@@ -80,7 +81,7 @@ def process_cmd(event):
     if cmd == "":
         return
 
-    log (cmd, 1, userinput=True)
+    event_log (cmd, "Console", mprefix=1, userinput=True)
 
     commands = {
         "hide":     hide_console,
@@ -93,23 +94,26 @@ def process_cmd(event):
     }
 
     func = commands.get (cmd, unknown_command)
+    func ()
 
 def terminate():
-    log("Shutting down console", 2)
+    event_log("Shutting down console", "Console", mprefix=2)
+    print ("Shuuting down")
     consoleGUI.after (1000, consoleGUI.destroy)
+    print ("destroyed console")
 
 def log_main_help():
-        log_array(help.help_main())
+    log_array(help.help_main())
 
 def unknown_command ():
-    log ("Unknown command. Type <help> for more information.", 2)
+    event_log ("Unknown command. Type <help> for more information.", "Console", mprefix=2)
     return
 
 """ Console appearance """
 
 def hide_console():
     consoleGUI.withdraw()
-    log ("Hid the console", 2)
+    event_log ("Hid the console", "Console", mprefix=2)
     create_trayicon ()
     trayicon.run ()
 
@@ -118,7 +122,7 @@ def restore_console ():
     #print ("console restored")
     consoleGUI.deiconify()
     delete_trayicon ()
-    log ("Console restored", 1)
+    event_log ("Console restored", "Console", mprefix=1)
 
 def clear_console ():
     console_output.configure(state='normal')
@@ -147,6 +151,49 @@ def trayicon_create_image(imagepath):
     image = Image.open (imagepath)
     return image
 
+""" Eventmanager """
+
+def configure_logger ():
+
+    global path
+
+    date = current_date()
+    time = current_time("-")
+
+    programstart = date + "_" + time
+    path = ".files\\logs\\" + programstart + ".log"
+
+    f = open(path, "a")
+    f.write ("--- This is the log-file for the GoogleAssistant2Windows, started on " + date + " at " + time + " ---\n")
+    f.close()
+
+def event_log (message, module, level = 1, time = 0, mprefix=0, userinput=False, guitext=0, log_only=False):
+
+    if level == 1:
+        str_level = "DEBUG"
+    elif level == 2:
+        str_level = "INFO"
+    elif level == 3:
+        str_level = "WARNING"
+    elif level == 4:
+        str_level = "ERROR"
+    elif level == 5:
+        str_level = "CRITICAL"
+    else:
+        str_level = "DEBUG"
+
+    if time == 0:
+        time = current_time()
+
+    message_log = "[" + str_level + " " + time + " " + module + "]\t\t" + message + "\n"
+    f = open (path, "a")
+    f.write (message_log)
+    f.close()
+
+    if log_only == False:
+        console_log(message, mprefix, time, userinput, guitext = queue_guitext)
+
+""" Execution functions """
 
 def run_console():
     create_console_GUI()
@@ -157,8 +204,17 @@ def run_console():
 
 
 if __name__ == '__main__':
-    run_console()
 
-    global guitext
-    guitext = q.get()
-    q.task_done()
+    configure_logger()
+
+    console_thread = Thread (target= run_console)
+    console_thread.start ()
+    event_log ("Started UI-Thread", "Console", log_only=True)
+
+    global queue_guitext
+    queue_guitext = q_output.get()
+    q_output.task_done()
+
+    event_log ("Console output is now accessible.", "Console", log_only=True)
+
+    console_thread.join()
