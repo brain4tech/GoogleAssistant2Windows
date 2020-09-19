348a5dc5c6c579224b32a57f74b73e67e5ae78f6
_Jefta_ <Jefta.deLeeuw@web.de>
2020-09-19 11:06:54 +0200

    Added commandlibrary-parser

--- src/cmdLibraryParser.py
+++ src/cmdLibraryParser.py
@@ -8,4 +8,32 @@ json_raw = f.read ()
 
 cmdLibrary = json.loads(json_raw)
 
-print (cmdLibrary)
+def getCommands ():
+    identifierlist = cmdLibrary['identifiers']
+
+    countCommands = len(identifierlist) -1
+
+    commandlist = []
+
+    for x in range (len(identifierlist)):
+        commandId = identifierlist[x]
+        commands = cmdLibrary [commandId]['commands']
+        for x in range (len(commands)):
+            command = commands[x]
+            commandlist.append([commandId, command])
+
+    return commandlist
+
+def getTargets (commandId):
+    try:
+        targetlist = cmdLibrary[commandId]['targets']
+        return targetlist
+    except:
+        pass
+
+def getCallFunc (commandId):
+    try:
+        targetlist = cmdLibrary[commandId]['callfunc']
+        return targetlist
+    except:
+        pass
