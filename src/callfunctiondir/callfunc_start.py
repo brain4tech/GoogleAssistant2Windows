32c1dfa9184a78af1c73b8e76c6f2571f2a52eca
_Jefta_ <Jefta.deLeeuw@web.de>
2020-09-29 21:43:15 +0200

    Improved comparison algorithm

--- src/callfunctiondir/callfunc_start.py
+++ src/callfunctiondir/callfunc_start.py
@@ -26,8 +26,6 @@ def getLocalStartmenu ():
 
 def getOtherApps ():
 
-    print (__file__)
-
     ppath = Path (__file__).parents[2]
     #print (ppath)
     path = Path (ppath / 'data' / 'callfuncfiles' / 'start_additional-programs.txt')
@@ -59,7 +57,7 @@ def optimizePathlist (rawpathlist):
 
     pathlist = []
 
-    substrings = getNoGoStrings()
+    substrings = getBlacklistStrings()
 
     for x in range (len(rawpathlist)):
 
@@ -77,7 +75,8 @@ def optimizePathlist (rawpathlist):
 
             if x in lnkfile:
 
-                #print ("substring <" + x + "> in " + lnkfile)
+                #print ("'" + lnkfile + "' got removed because it contained <" + x + ">")
+                #print (strpath)
                 substringInLinkfile = True
 
         if substringInLinkfile != True:
@@ -95,10 +94,10 @@ def sortPathlist (pathlist):
         #print (pathlist [x][0])
     return pathlist
 
-def getNoGoStrings ():
+def getBlacklistStrings ():
     ppath = Path (__file__).parents[2]
     #print (ppath)
-    path = Path (ppath / 'data' / 'callfuncfiles' / 'start_nogo-substrings.txt')
+    path = Path (ppath / 'data' / 'callfuncfiles' / 'start_blacklist-substrings.txt')
     file = open(path, "r", encoding='utf8')
     strings = file.readlines()
 
@@ -115,13 +114,55 @@ def comparisonAlgorithm1 (array, input):
         if input in array[x][0]:
             #print ("true")
             #print (array[x][1])
-            path = array[x][1]
-            return path
+
+            result = [array[x][0], array[x][1]]
+            print (result)
+
+            return result
 
     return False
 
 def comparisonAlgorithm2 (array, input):
-    input.split()
+    input = input.split()
+
+    print (input)
+
+    summarylist = []
+
+    for x in range(len(array)):
+
+        indexsplit =  array[x][0].split()
+        #print (indexsplit)
+
+        localcounter = 0
+
+        for y in range (len(input)):
+            for z  in range (len(indexsplit)):
+
+                inputindex = input[y]
+                listindex = indexsplit[z]
+
+                if inputindex in listindex:
+
+                    #print (indexsplit)
+                    print ("MATCH: Input <" + inputindex + "> fits to <" + listindex + "> in '" + array[x][0] + "'")
+
+                    localcounter = localcounter + 1
+
+        if localcounter > 0:
+            summarylist.append([localcounter, x, array[x][0]]) #counter, index in array
+
+    if summarylist == []:
+        return False
+
+    summarylist.sort(reverse=True)
+
+    print (summarylist)
+
+    result = [array[summarylist[0][1]][0], array[summarylist[0][1]][1]]
+
+    return result
+
 
 
 def getPathlist ():
@@ -148,9 +189,7 @@ def getPathlist ():
 
     return pathlist_sorted
 
-
-def cf_start (input=None):      #open a program or a file
-
+def checkForProgram (input):
     input = str(input).lower()
     if input == "" :
         return
@@ -163,7 +202,44 @@ def cf_start (input=None):      #open a program or a file
     #print (result)
 
     if result != False:
-        win32api.ShellExecute (0, None, result, None, None, 1)
+        print ("MATCH! <" +input + "> has a match with:", result[0] + ". Path to program:", result[1])
+    else:
+        print ("ERROR: No match found for <" + input +">")
+
+def printPathlist (index = None):
+
+    array = getPathlist()
+
+    if index != None:
+
+        try:
+
+            print ("Name:", array[index][0])
+            print ("Path:", array[index][1])
+        except Exception as e:
+            print ("ERROR", e)
+
+    else:
+
+        for x in range(len(array)):
+            print (array[x][0],"-->", array[x][1])
+
+def cf_start (input=None):      #open a program or a file
+
+    input = str(input).lower()
+    if input == "" :
+        return
+
+    pathlist = getPathlist()
+    #print (pathlist)
+
+    #check if input is in pathlist
+    result = comparisonAlgorithm2(pathlist, input)
+    #print (result)
+
+    if result != False:
+        #win32api.ShellExecute (0, None, result[1], None, None, 1)
+        print ("Match")
     else:
         print ("No match found")
 
