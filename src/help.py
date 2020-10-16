5663ea8f73ec98004842804789f6f76518a1978b
_Jefta_ <Jefta.deLeeuw@web.de>
2020-10-16 18:47:26 +0200

    removed help.py as it is not neccessary anymore

--- src/help.py
+++
@@ -1,6 +0,0 @@
-def help_main ():
-    help = [">   The following general commands are documented:",
-        "  quit\tShuts down the application",
-        "  hide\tHides the console and creates a trayicon.\n  \tYou can restore the console by right-clicking on this trayicon.",
-        "  clear\tClears the console output"]
-    return help
